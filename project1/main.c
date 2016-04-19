0ef7a198a36059667bcd38eb720f67a7f62ebee0
STEFFEN JOHN OLAV MITCHELL <s4353096@student.uq.edu.au>
2016-04-20 03:23:43 +1000

    Cleaned up main mostly.

--- project1/main.c
+++ project1/main.c
@@ -1,11 +1,11 @@
 /**
 ******************************************************************************
-* @file    stage3/main.c
+* @file    project1/main.c
 * @author  Steffen Mitchell
 * @date    10-January-2015
-* @brief   Prac 1 Template C main file - BCD timer and press counter.
+* @brief   Project 1 main file.
 *
-*			 REFERENCES: ex1_led, ex2_gpio, ex3_gpio, ex11_character
+*			 REFERENCES: 
 ******************************************************************************
 */
 
@@ -17,7 +17,6 @@
 #include "s4353096_joystick.h"
 #include "s4353096_hamming.h"
 #include "s4353096_radio.h"
-//#include "s4353096_radio.h"
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
@@ -25,11 +24,11 @@
 #define S4353096_JOYSTICK 1
 #define S4353096_TERMINAL 0
 #define S4353096_LASER_TRANSMIT 2
+#define S4353096_RADIO 3
 #define LASER_WAVE_GEN_1_PIN BRD_D1_PIN
 #define LASER_WAVE_GEN_1_GPIO_PORT BRD_D1_GPIO_PORT
 #define __LASER_WAVE_GEN_1_GPIO_CLK() __BRD_D1_GPIO_CLK()
 #define LASER_WAVE_GEN_1_EXTI_IRQ BRD_D1_EXTI_IRQ
-
 TIM_HandleTypeDef TIM_Init;
 TIM_IC_InitTypeDef  TIM_ICInitStructure;
 uint16_t counter_value = 64;
@@ -53,6 +52,7 @@ struct Variables {
 	int recieve_element;
 	int recieve_period;
 	int current_bit;
+	int recieve_flag;
 };
 struct Variables *vars;
 unsigned int x_value;
@@ -74,15 +74,10 @@ void s4353096_laser_transmit_bit(int bit);
 void tim3_irqhandler (void);
 int approximately_equal(unsigned int number, unsigned int check_number);
 void manchester_decode(void);
-
 void main(void) {
-
 	BRD_init();	//Initalise NP2
 	Hardware_init();
 	HAL_Delay(3000);
-		//Initalise hardware modules
-
-	/* Main processing loop */
 	struct PanTilt pantiltvars;
 	pantilt = &pantiltvars;
 	pantilt->write_angles = 0;
@@ -100,18 +95,19 @@ void main(void) {
 	vars->transmit_frequency = 1000;
 	vars->period_multiplyer = ((1.000000/vars->transmit_frequency)*500)*0.998;
 	vars->recieve_element = 0;
-	//debug_printf("\n%f\n", variables.period_multiplyer);
+	vars->recieve_flag = 0;
 	Pb_init();
-	//HAL_Delay(1000);
-	for(int i=0; i<16; i++) {
-		debug_printf("%d", !!((vars->encoded_char >> i) & 0x1));
-	}
 	s4353096_radio_setchan(s4353096_chan);
 	s4353096_radio_settxaddress(s4353096_tx_addr);
 	s4353096_radio_setrxaddress(s4353096_rx_addr);
   while (1) {
 		s4353096_radio_setfsmrx();
 		s4353096_radio_fsmprocessing();
+		if (vars->recieve_flag == 1) {
+			manchester_decode();
+			vars->recieve_flag = 0;
+		}
+		if (mode == S4353096_RADIO) {
 		/*Processes the current fsm state*/
 		RxChar = debug_getc();
 		if (RxChar != '\0') {
@@ -153,6 +149,7 @@ void main(void) {
 		} else {
 
 		}
+	}
 		if (pantilt->write_angles == 1) {
 			s4353096_pantilt_angle_write(1, pantilt->set_angle_pan);
 			s4353096_pantilt_angle_write(0, pantilt->set_angle_tilt);
@@ -160,12 +157,7 @@ void main(void) {
 		}
 		if (((HAL_GetTick()/10000) % 100) == 0) {
 			debug_printf("\nPan: %d Tlit: %d\n", pantilt->set_angle_pan, pantilt->set_angle_tilt);
-			for (int i = 0; i < vars->recieve_element; i++) {
-				debug_printf("Recieve edge %d : %d\n", i, vars->recieve[i]);
-			}
-			manchester_decode();
-			//BRD_LEDToggle();
-		}//
+		}
 		if (pantilt->read_angles == 1) { /*Delay for 1 second or setup to delay for 0.2 seconds and set angle to += or -= 1 each time*/
 			if (mode == S4353096_JOYSTICK) {
 				y_value = s4353096_joystick_y_read();
@@ -216,13 +208,8 @@ void main(void) {
 	}
 }
 
-/**
-  * @brief  Initialise Hardware
-  * @param  None
-  * @retval None
-  */
+/*Initialise relevent hardware*/
 void Hardware_init(void) {
-
 	GPIO_InitTypeDef  GPIO_InitStructure;
 	unsigned short PrescalerValue;
 	BRD_LEDInit();		//Initialise Blue LED
@@ -232,28 +219,22 @@ void Hardware_init(void) {
 	/* Configure D1 for output of square wave signal */
 	s4353096_pantilt_init();
 	s4353096_radio_init();
-	//
-	//Pb_init();
 }
 void Pb_init(void) {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	/* Enable PB clock */
 		__BRD_PB_GPIO_CLK();
-
 	/* Set priority of PB Interrupt [0 (HIGH priority) to 15(LOW priority)] */
 	HAL_NVIC_SetPriority(BRD_PB_EXTI_IRQ, 4, 0);	//Set Main priority ot 10 and sub-priority ot 0.
-
 	//Enable PB interrupt and interrupt vector for pin DO
 	NVIC_SetVector(BRD_PB_EXTI_IRQ, (uint32_t)&exti_pb_irqhandler);
 	NVIC_EnableIRQ(BRD_PB_EXTI_IRQ);
-
-		/* Configure PB pin as pull down input */
+	/* Configure PB pin as pull down input */
 	GPIO_InitStructure.Pin = BRD_PB_PIN;				//Pin
 		GPIO_InitStructure.Mode = GPIO_MODE_IT_RISING;		//interrupt Mode
 		GPIO_InitStructure.Pull = GPIO_PULLUP;			//Enable Pull up, down or no pull resister
 		GPIO_InitStructure.Speed = GPIO_SPEED_FAST;			//Pin latency
 		HAL_GPIO_Init(BRD_PB_GPIO_PORT, &GPIO_InitStructure);	//Initialise Pin
-
 		/*Set up Interrupt timer for laser transmit and pan tilt*/
 	  __PANTILT_IR_TIMER_CLK();
 		/* Compute the prescaler value for 50Khz */
@@ -266,7 +247,7 @@ void Pb_init(void) {
 	  TIM_Init.Init.ClockDivision = 0;			//Set clock division
 		TIM_Init.Init.RepetitionCounter = 0;	// Set Reload Value
 	  TIM_Init.Init.CounterMode = TIM_COUNTERMODE_UP;	//Set timer to count up.
-
+		/*Initialise Laser Transmit Port*/
 		__LASER_WAVE_GEN_1_GPIO_CLK();
 		GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
 		GPIO_InitStructure.Pull = GPIO_PULLDOWN;
@@ -274,32 +255,20 @@ void Pb_init(void) {
 		GPIO_InitStructure.Pin = LASER_WAVE_GEN_1_PIN;
 		HAL_GPIO_Init(LASER_WAVE_GEN_1_GPIO_PORT, &GPIO_InitStructure);
 		HAL_Delay(500);
-		/* Initialise Timer */
+		/* Initialise General interrupt Timer  */
 		HAL_TIM_Base_Init(&TIM_Init);
-
 		/* Set priority of Timer 2 update Interrupt [0 (HIGH priority) to 15(LOW priority)] */
 		/* 	DO NOT SET INTERRUPT PRIORITY HIGHER THAN 3 */
 		HAL_NVIC_SetPriority(PANTILT_TIM_IRQn, 10, 0);		//Set Main priority ot 10 and sub-priority ot 0.
-
 		/* Enable timer update interrupt and interrupt vector for Timer  */
 		NVIC_SetVector(PANTILT_TIM_IRQn, (uint32_t)&s4353096_general_irqhandler);
 		NVIC_EnableIRQ(PANTILT_TIM_IRQn);
 		/*Start the timer*/
 		HAL_TIM_Base_Start_IT(&TIM_Init);
-		/*Set up Laser transmit pins*/
-		/*__LASER_WAVE_GEN_1_GPIO_CLK();
-		GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-		GPIO_InitStructure.Pull = GPIO_PULLDOWN;
-		GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
-		GPIO_InitStructure.Pin = LASER_WAVE_GEN_1_PIN;
-		HAL_GPIO_Init(LASER_WAVE_GEN_1_GPIO_PORT, &GPIO_InitStructure);*/
-
-		/*Input capture*/
+		/*Initialise Input capture*/
 		__TIM3_CLK_ENABLE();
-
 		/* Enable the D0 Clock */
 		__BRD_D0_GPIO_CLK();
-
 		/* Configure the D0 pin with TIM3 input capture */
 	GPIO_InitStructure.Pin = BRD_D0_PIN;				//Pin
 		GPIO_InitStructure.Mode =GPIO_MODE_AF_PP; 		//Set mode to be output alternate
@@ -307,10 +276,8 @@ void Pb_init(void) {
 		GPIO_InitStructure.Speed = GPIO_SPEED_FAST;			//Pin latency
 	GPIO_InitStructure.Alternate = GPIO_AF2_TIM3;	//Set alternate function to be timer 2
 		HAL_GPIO_Init(BRD_D0_GPIO_PORT, &GPIO_InitStructure);	//Initialise Pin
-
-	/* Compute the prescaler value. SystemCoreClock = 168000000 - set for 50Khz clock */
+	/* Compute the prescaler value. SystemCoreClock = 168000000 - set for 500Khz clock */
 		PrescalerValue = (uint16_t) ((SystemCoreClock /2) / 500000) - 1;
-
 	/* Configure Timer 3 settings */
 	TIM_Init.Instance = TIM3;					//Enable Timer 3
 		TIM_Init.Init.Period = 2*500000;			//Set for 100ms (10Hz) period
@@ -318,33 +285,30 @@ void Pb_init(void) {
 		TIM_Init.Init.ClockDivision = 0;			//Set clock division
 	TIM_Init.Init.RepetitionCounter = 0; 		// Set Reload Value
 		TIM_Init.Init.CounterMode = TIM_COUNTERMODE_UP;	//Set timer to count up.
-
 	/* Configure TIM3 Input capture */
 		TIM_ICInitStructure.ICPolarity = TIM_ICPOLARITY_RISING;			//Set to trigger on rising edge
 		TIM_ICInitStructure.ICSelection = TIM_ICSELECTION_DIRECTTI;
 		TIM_ICInitStructure.ICPrescaler = TIM_ICPSC_DIV1;
 		TIM_ICInitStructure.ICFilter = 0;
-
 	/* Set priority of Timer 3 Interrupt [0 (HIGH priority) to 15(LOW priority)] */
 	HAL_NVIC_SetPriority(TIM3_IRQn, 10, 0);	//Set Main priority ot 10 and sub-priority ot 0.
-
 	//Enable Timer 3 interrupt and interrupt vector
 	NVIC_SetVector(TIM3_IRQn, (uint32_t)&tim3_irqhandler);
 	NVIC_EnableIRQ(TIM3_IRQn);
-
 	/* Enable input capture for Timer 3, channel 2 */
 	HAL_TIM_IC_Init(&TIM_Init);
 	HAL_TIM_IC_ConfigChannel(&TIM_Init, &TIM_ICInitStructure, TIM_CHANNEL_2);
-
 	/* Start Input Capture */
 	HAL_TIM_IC_Start_IT(&TIM_Init, TIM_CHANNEL_2);
 }
+/*Interrupt handler for on board push button*/
 void exti_pb_irqhandler(void) {
-	//debug_printf("Hey\n");
 	HAL_GPIO_EXTI_IRQHandler(BRD_PB_PIN);
 	mode = !mode;
 }
-
+/*General Interrupt handler which handles interrupts for Laser trasnmit and
+	writing angles to pan and tilt. The interrupt is set to occur every half
+	manchester bit period for the purposes of transmitting manchester using GPIO*/
 void s4353096_general_irqhandler(void) {
   TIM_Init.Instance = PANTILT_IR_TIM;
   __HAL_TIM_CLEAR_IT(&TIM_Init, TIM_IT_UPDATE);
@@ -355,7 +319,6 @@ void s4353096_general_irqhandler(void) {
 			s4353096_laser_transmit_bit(1);
 		} else if (vars->bit_count == 10 || vars->bit_count == 21) {
 				s4353096_laser_transmit_bit(0);
-			//HAL_Delay(10);
 		} else if ((vars->bit_count == 22) && (vars->bit_half == 1)) {
 			//Stop transmitting and reset values
 			HAL_GPIO_WritePin(LASER_WAVE_GEN_1_GPIO_PORT, LASER_WAVE_GEN_1_PIN, 0);
@@ -376,6 +339,7 @@ void s4353096_general_irqhandler(void) {
 		vars->count = 0;
 	}
 }
+/*Handles the transmission of bits in a manchester encoding*/
 void s4353096_laser_transmit_bit(int bit) {
   if (vars->bit_half == 1) {
 		HAL_GPIO_WritePin(LASER_WAVE_GEN_1_GPIO_PORT, LASER_WAVE_GEN_1_PIN, !(bit));
@@ -386,6 +350,7 @@ void s4353096_laser_transmit_bit(int bit) {
 		vars->bit_half--;
 	}
 }
+/*The Input Capture interrupt handler which is used to recieve a laser transmission*/
 void tim3_irqhandler (void) {
 	unsigned int input_capture_value;
 	TIM_Init.Instance = TIM3;					//Enable Timer 3
@@ -396,11 +361,8 @@ void tim3_irqhandler (void) {
 		TIM_Init.Init.CounterMode = TIM_COUNTERMODE_UP;	//Set timer to count up.
 	//Clear Input Capture Flag
 	__HAL_TIM_CLEAR_IT(&TIM_Init, TIM_IT_TRIGGER);
-
-	/* Toggle LED */
-	//BRD_LEDToggle();
-
   	/* Read and display the Input Capture value of Timer 3, channel 2 */
+		vars->recieve_flag = 1;
   	input_capture_value = HAL_TIM_ReadCapturedValue(&TIM_Init, TIM_CHANNEL_2);
 		vars->recieve[vars->recieve_element] = input_capture_value;
 		vars->recieve_element++;
@@ -417,9 +379,9 @@ void tim3_irqhandler (void) {
 		HAL_TIM_IC_ConfigChannel(&TIM_Init, &TIM_ICInitStructure, TIM_CHANNEL_2);
 		/* Start Input Capture */
 		HAL_TIM_IC_Start_IT(&TIM_Init, TIM_CHANNEL_2);
-  	//debug_printf("Input capture 2: %d RE = %d\n\r", input_capture_value);
-
 }
+/*Checks whether an unsigned int is approximately equal to another unsigned int
+	by a margin of approximately 20%, returns True if it is, else returns false*/
 int approximately_equal(unsigned int number, unsigned int check_number) {
 	if ((number < ((check_number)*1.2)) && (number > ((check_number)*0.8) )) {
 		return 1;
@@ -427,6 +389,8 @@ int approximately_equal(unsigned int number, unsigned int check_number) {
 		return 0;
 	}
 }
+/*Converts the recieved input captures into bits which are then passed through
+	the hamming_byte_decoder to extract data bits and correct/notify of any bit errors*/
 void manchester_decode(void) {
 	int j = 0;
 	vars->current_bit = 1;
@@ -481,7 +445,7 @@ void manchester_decode(void) {
 	debug_printf("ERROR2\n");
 	}
 	/*lower half byte*/
-			/*Convert recieved_decoded to MSB and remove start and stop bits*/
+	/*Convert recieve_decoded to MSB and remove start and stop bits*/
 	for (int k = 0; k < 8; k++) {
 		if (k == 7) {
 			lower_byte = (lower_byte ^ vars->recieve_decoded[9-k]);
@@ -489,8 +453,9 @@ void manchester_decode(void) {
 			lower_byte = (lower_byte ^ vars->recieve_decoded[9-k]) << 1;
 		}
 	}
+	/*Checks if start bits for second recieved byte is present*/
 	if ((vars->recieve_decoded[11] == 1) && (vars->recieve_decoded[12] == 1)) {//Then start bits for upper are present
-		/*Convert recieved_decoded to MSB and remove start and stop bits*/
+		/*Convert recieve_decoded to MSB and remove start and stop bits*/
 		for (int m = 0; m < 8; m++) {
 			if (m == 7) {
 				upper_byte = (upper_byte ^ vars->recieve_decoded[20-m]);
@@ -502,47 +467,5 @@ void manchester_decode(void) {
 		debug_printf("ERROR3\n");
 		//ERROR No start bits recieved
 	}	/*Convert recieve_decoded to MSB and remove start and stop bits*/
-	/*debug_printf("Recieve_decoded is ");
-	for(int p = 0; p < 22; p++) {
-		debug_printf("%d", vars->recieve_decoded[p]);
-	}*/
-	debug_printf("\nUpper is ");
-	for(int g = 0; g<8; g++) {
-		debug_printf("%d", !!((upper_byte << g) & 0x80));
-	}
-	debug_printf("\nLower is ");
-	for(int t=0; t<8; t++) {
-		debug_printf("%d", !!((lower_byte << t) & 0x80));
-	}
 	hamming_byte_decoder(lower_byte, upper_byte);
 }
-/*void s4353096_pantilt_irqhandler(void) {
-  TIM_Init.Instance = PANTILT_IR_TIM;
-  __HAL_TIM_CLEAR_IT(&TIM_Init, TIM_IT_UPDATE);
-	//interrupt_time = HAL_GetTick();
-	pantilt->read_angles = 1;
-  pantilt->write_angles = 1;
-}*/
-/*void exti_a2_interrupt_handler(void) {
-	HAL_GPIO_EXTI_IRQHandler(BRD_A2_PIN);
-}*/
-	//int button_state = s4353096_joystick_z_read();
-	//q = 0;
-	//while (q = 0) {
-	//BRD_LEDToggle();
-		/*if (reading != last_button_state) {
-			last_Debounce_Time = HAL_GetTick()/1000;
-		}*/
-	/*	if ((HAL_GetTick()/1000 - last_Debounce_Time) > 10) {
-				if (button_state == 1) {
-					set_angle = set_angle + direction_multiplier*10;
-					debug_printf("%d\n", set_angle);
-					q = 1;
-				//HAL_GPIO_EXTI_IRQHandler(BRD_A2_PIN);
-				}
-		}
-		//last_button_state = reading;
-	}
-}*/
-	/* Speed up the counter by reducing the delay value */
-//}
